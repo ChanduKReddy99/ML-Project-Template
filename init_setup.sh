e713d5f4c4aa9436b7e75617de39c340bcf18964
ChanduKReddy99 <75457951+ChanduKReddy99@users.noreply.github.com>
2022-12-02 22:47:42 +0530

    init_setup.sh updated

--- init_setup.sh
+++ init_setup.sh
@@ -1,3 +1,14 @@
-conda create -p .venv python=3.8 -y
-source activate .venv
+echo [$(date)]: "START"
+echo [$(date)]: "creating conda virtual env"
+conda create --prefix ./venv python=3.8 -y
+echo [$(date)]: "activating conda virtual env"
+source activate ./venv
+echo [$(date)]: "installing dev requirements"
 pip install -r requirements_dev.txt
+echo [$(date)]: "END"
+
+
+
+# conda create -p .venv python=3.8 -y
+# source activate .venv
+# pip install -r requirements_dev.txt
