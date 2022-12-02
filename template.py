24167400b7d01c5c681ff1f0a1ee91dc17b7f9b2
ChanduKReddy99 <75457951+ChanduKReddy99@users.noreply.github.com>
2022-12-02 15:44:49 +0000

    project skeleton created using template.py file

---
+++ template.py
@@ -0,0 +1,53 @@
+""" creating template file to create project skeleton """
+
+import os
+import logging
+from pathlib import Path
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    filemode="a"
+)
+
+while True:
+    project_name= input("Enter the project_name: ")
+    if project_name != "":
+        break
+
+# lazy formatting in logging funcs (pylint preferred way to do)
+logging.info(" creating project by name: %s", project_name)
+# logging.inf("creating project by name: {project_name}")
+
+# list of files:
+list_of_files= [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    "tests/__init__.py",
+    "tests/unit/__init__.py",
+    "tests/integration/__init__.py",
+    "requirements_dev.txt",
+    "requirements.txt",
+    "init_setup.sh",
+    "setup.py",
+    "setup.cfg",
+    "pyproject.toml",
+    "tox.ini",
+    "Makefile"
+
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename= os.path.split(filepath)
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(" creating a directory at: %s for file name: %s", filedir, filename)
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+        with open(filepath, "w", encoding="utf-8") as f:
+            pass
+        logging.info("creating a new file: path%s at : %s", filename, filepath)
+    else:
+        logging.info(" file already present at: %s", filepath)
